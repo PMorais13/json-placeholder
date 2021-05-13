eb8320f505ee5b75e0c7a068690b1ce1f1095721
PMorais13 <phmscout@gmail.com>
2021-05-13 12:41:56 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './model/post.interface';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto-um';
+
+  post!: Array<Post>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getPost();
+  }
+  getPost() {
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+      .then(response => response.json())
+      .then(json => {
+        this.post = json;
+      }
+      )
+  }
 }
+
+
+
